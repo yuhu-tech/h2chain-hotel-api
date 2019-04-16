3053eff704855a2a3bf45235162a61cdbc067fce
root <18015565550@189.cn>
2019-04-16 18:28:57 +0800

    add modifyorders into it

--- server/src/resolvers/mutation/order.js
+++ server/src/resolvers/mutation/order.js
@@ -9,6 +9,7 @@ const client = new services.MutationClient('127.0.0.1:50051',grpc.credentials.cr
 const order = {
   async createorder(parent,args,ctx,info) {
     const id = getUserId(ctx) //retrieve userid to put it into hotelId
+    console.log(id)
     const users  =  await ctx.prismaHr.users({where:{name:args.createorder.advisername}})
     var request = new messages.CreateRequest();
     request.setHotelid(id)  //hotelid can be received for ctx utils
@@ -20,12 +21,25 @@ const order = {
     request.setCountmale(args.createorder.male)
     request.setCountfemale(args.createorder.female)
     request.setMode(args.createorder.mode)
-    
     client.createOrder(request, function(err, response) {
     console.log(response);
     });
    },
   
+  async modifyorder(parent,args,crx,info){
+    var request = new messages.ModifyRequest();
+        request.setOrderid(args.modifiedorder.orderid);
+        request.setDatechanged(args.modifiedorder.changeddatetime);
+        request.setDurationchanged(args.modifiedorder.changedduration);             //为修改的数据，传 -1 或者 原数据值
+        request.setCountchanged(args.modifiedorder.changedcount);
+        request.setCountmalechanged(args.modifiedorder.changedmale);
+        request.setMode(args.modifiedorder.changedmode);
+    client.modifyOrder(request,function(err,response){
+        console.log(response.array)
+    });
+  },
+
+ 
   async closeorder(parent,args,ctx,info) {
     var client = new services.MutationClient('127.0.0.1:50051',grpc.credentials.createInsecure());
     var request = new messages.CloseRequest();
