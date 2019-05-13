11753f000c0c311a8396540c98e3a491b94348c5
ted <zhuwentao@yuhu.tech>
2019-05-13 13:55:35 +0800

    msgv1.0.1

--- resolvers/mutation/order.js
+++ resolvers/mutation/order.js
@@ -3,6 +3,11 @@ const messages = require('../../grpc/mutation/mutation_pb');
 const services = require('../../grpc/mutation/mutation_grpc_pb');
 const grpc = require('grpc');
 const config = require('../../conf/config')
+const sd = require('silly-datetime')
+const sendtoa = require('../../msg/msghandle/sendmsg/advisermsg')
+const sendtop = require('../../msg/msghandle/sendmsg/ptmsg')
+const formid = require('../../msg/msghandle/formid/redis')
+
 const client = new services.MutationClient(config.localip, grpc.credentials.createInsecure());
 
 const order = {
@@ -20,6 +25,14 @@ const order = {
     request.setMode(args.createorder.mode)
     client.createOrder(request, function (err, response) {
     });
+
+    // set formid which is created when hotel create order
+    var userId = id
+    var orderId = ''
+    var formId = ''
+    var setRes = await formid.setFormId(userId, orderId, formId)
+    console.log('set formid after creating :', setRes)
+
   },
 
   async modifyorder(parent, args, ctx, info) {
@@ -37,6 +50,44 @@ const order = {
     request.setTargetstatus(4);                           // PT 目标状态 筛选条件，不同传 -1  
     request.setSourcestatus(1);                           // PT 原始状态  
     client.modifyPTOfOrder(request, function (err, response) { console.log(response.array) });
+
+    // set formid which is created when hotel modify order
+    var userId = ''
+    var orderId = ''
+    var formId = ''
+    var setRes = await formid.setFormId(userId, orderId, formId)
+    console.log('set formid after modifying :', setRes)
+
+    // send msg to adviser after modifying
+    var AdviserMsgData = {
+      userId: 'adviserid',
+      orderId: 'orderid',
+      openId: 'adviser openid',
+      num: 4,
+      content: {
+        keyword1: '',
+        keyword2: sd.format(new Date(), 'YYYY/MM/DD HH:mm'),
+        keyword3: '',
+      }
+    }
+    var sendARes = await sendtoa.sendTemplateMsgToAdviser(AdviserMsgData)
+    console.log('send msg to adviser after modifying', sendARes)
+
+    // send msg to registried pts after modifying
+    // there should be a pt list , have to use for() to handle 
+    var PtMsgData = {
+      userId: 'ptid',
+      orderId: 'orderid',
+      openId: 'pt openid',
+      num: 1,
+      content: {
+        keyword1: '',
+        keyword2: sd.format(new Date(), 'YYYY/MM/DD HH:mm'),
+        keyword3: '',
+      }
+    }
+    var sendPRes = await sendtop.sendTemplateMsgToPt(PtMsgData)
+    console.log('send msg to pt after modifying', sendPRes)
   },
 
 
@@ -46,6 +97,37 @@ const order = {
     request.setOrderid(args.orderid)
     client.closeOrder(request, function (err, response) {
     })
+
+    // send msg to adviser after closing
+    var AdviserMsgData = {
+      userId: 'adviserid',
+      orderId: 'orderid',
+      openId: 'adviser openid',
+      num: 6,
+      content: {
+        keyword1: '',
+        keyword2: '',
+        keyword3: sd.format(new Date(), 'YYYY/MM/DD HH:mm'),
+      }
+    }
+    var sendARes = await sendtoa.sendTemplateMsgToAdviser(AdviserMsgData)
+    console.log('send msg to adviser after modifying', sendARes)
+
+    // send msg to registried pts after closing
+    // there should be a pt list , have to use for() to handle 
+    var PtMsgData = {
+      userId: 'ptid',
+      orderId: 'orderid',
+      openId: 'pt openid',
+      num: 3,
+      content: {
+        keyword1: '',
+        keyword2: sd.format(new Date(), 'YYYY/MM/DD HH:mm'),
+        keyword3: '',
+      }
+    }
+    var sendPRes = await sendtop.sendTemplateMsgToPt(PtMsgData)
+    console.log('send msg to pt after modifying', sendPRes)
   }
 }
 
